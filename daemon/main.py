c697d7252aa067debed52c25c51d2c07396efc8e
Bogdan Nitica <bogdan.nitica99@gmail.com>
2020-01-15 11:45:47 +0200

    addresing the review comments

--- daemon/main.py
+++ daemon/main.py
@@ -4,15 +4,15 @@ from collections import defaultdict
 
 class Mutex:
 	def __init__(self):
-		self.nume_actual = None
-		self.procese = set()
+		self.actual_name = None
+		self.process = set()
 		self.coada = []
 
 mutecsii = defaultdict(Mutex)
 
-def send_message(socket, adresa_client, message):
+def send_message(socket, addres_client, message):
 	socket.send_multipart([
-		adresa_client,
+		address_client,
 		b'',
 		message
 	])
@@ -28,44 +28,53 @@ def main():
 	socket.bind("tcp://*:5555")
 
 	while True:
-		adresa_client, empty, msg = socket.recv_multipart() #nume client, empty, mesaj
+		address_client, empty, msg = socket.recv_multipart() #nume client, empty, mesaj
 
-		nume_mutex = msg[1:]
+		name_mutex = msg[1:]
+
+		my_mutex = mutecsii[name_mutex]
 
 		if msg[0] == 'O': #Open
-			if adresa_client in mutecsii[nume_mutex].procese:
-				send_message(socket, adresa_client, "b'Nu este Ok'")
+			if address_client in my_mutex.procese:
+				send_message(socket, address_client, b'Nu este Ok')
 			else:
-				mutecsii[mutex].procese.append(nume_client)
-				send_message(socket, adresa_client, "b'Ok'")
+				my_mutex.procese.append(nume_client)
+				send_message(socket, address_client, b'Ok')
 
-		if msg[0] == 'C':	#Close
-			if adresa_client in mutecsii[nume_mutex].procese:
-				mutecsii.procese.delete(adresa_client)
-				send_message(socket, adresa_client, "b'S-a sters'")
+		elif msg[0] == 'C':	#Close
+			if address_client in my_mutex.process:
+				mutecsii.process.delete(address_client)
+				send_message(socket, address_client, b'S-a sters')
+				if not my_mutex.process:
+					del mutecsii[name_mutex]
+
 			else:
-				send_message(socket, adresa_client, "b'Nu exista adresa specificata'")
+				send_message(socket, address_client, b'Nu exista adresa specificata')
 
-		if msg[0] == 'L':	#Lock
-			if adresa_client in mutecsii[nume_mutex].procese:
-				if mutecsii[nume_mutex].nume_actual == None:
-					send_message(socket, adresa_client, "b'Ai dat Lock'")
+		elif msg[0] == 'L':	#Lock
+			if address_client in my_mutex.procese:
+				if my_mutex.actual_name == None:
+					send_message(socket, address_client, b'Ai dat Lock')
 				else:
-					mutecsii[nume_mutex].coada.append(nume_client)
+					my_mutex.coada.append(nume_client)
 			else:
-				send_message(socket, adresa_client, "b'Nu exista adresa specificata'")
+				send_message(socket, address_client, b'Nu exista adresa specificata')
 
-		if msg[0] == 'U':	#Unlock
-			if adresa_client in mutecsii[nume_mutex].procese:
-				if mutecsii[nume_mutex].nume_actual == nume_client:
-					send_message(socket, adresa_client, "b'Ai dat Unlock'")
-					next_guy = mutecsii[nume_mutex].coada.pop(0)
-					mutecsii[nume_mutex].nume_actual = next_guy
-					send_message(socket, adresa_client, "b'Ai dat Lock'")
+		elif msg[0] == 'U':	#Unlock
+			if addres_client in mutecsii[nume_mutex].procese:
+				if my_mutex.actual_name == nume_client:
+					send_message(socket, address_client, b'Ai dat Unlock')
+					next_guy = my_mutex.coada.pop(0)
+					my_mutex.actual_name = next_guy
+					send_message(socket, address_client, b'Ai dat Lock')
 				else:
-					send_message(socket, adresa_client, "b'Nume client != nume actual'")
+					send_message(socket, address_client, b'Nume client != nume actual')
 			else:
-				send_message(socket, adresa_client, "b'Nu exista adresa specificata'")
+				send_message(socket, address_client, b'Nu exista adresa specificata')
+
+		else:
+			print("Ce vrei de la viata mea!!!?!!?")
+			send_message(socket, address_client, b'Nu ai ales bine. Mai incearca')
 
 
 if __name__ == '__main__':
