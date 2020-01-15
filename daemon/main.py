fa16c650952f06af9979da34433ed75d41ff42a3
Bogdan Nitica <bogdan.nitica99@gmail.com>
2020-01-15 12:26:25 +0200

    solved typo problems

--- daemon/main.py
+++ daemon/main.py
@@ -10,7 +10,7 @@ class Mutex:
 
 mutecsii = defaultdict(Mutex)
 
-def send_message(socket, addres_client, message):
+def send_message(socket, address_client, message):
 	socket.send_multipart([
 		address_client,
 		b'',
@@ -30,20 +30,27 @@ def main():
 	while True:
 		address_client, empty, msg = socket.recv_multipart() #nume client, empty, mesaj
 
+		msg = msg.decode('UTF-8')
+
 		name_mutex = msg[1:]
+
+		print("Toni incearca sa se conecteze pe laptopul meu")
+		print(address_client, msg)
 
 		my_mutex = mutecsii[name_mutex]
 
+		print(msg[0])
+
 		if msg[0] == 'O': #Open
-			if address_client in my_mutex.procese:
+			if address_client in my_mutex.process:
 				send_message(socket, address_client, b'Nu este Ok')
 			else:
-				my_mutex.procese.append(nume_client)
+				my_mutex.process.add(address_client)
 				send_message(socket, address_client, b'Ok')
 
 		elif msg[0] == 'C':	#Close
 			if address_client in my_mutex.process:
-				mutecsii.process.delete(address_client)
+				my_mutex.process.remove(address_client)
 				send_message(socket, address_client, b'S-a sters')
 				if not my_mutex.process:
 					del mutecsii[name_mutex]
@@ -52,21 +59,25 @@ def main():
 				send_message(socket, address_client, b'Nu exista adresa specificata')
 
 		elif msg[0] == 'L':	#Lock
-			if address_client in my_mutex.procese:
+			if address_client in my_mutex.process:
 				if my_mutex.actual_name == None:
 					send_message(socket, address_client, b'Ai dat Lock')
 				else:
-					my_mutex.coada.append(nume_client)
+					my_mutex.coada.append(addres_client)
 			else:
 				send_message(socket, address_client, b'Nu exista adresa specificata')
 
 		elif msg[0] == 'U':	#Unlock
-			if addres_client in mutecsii[nume_mutex].procese:
-				if my_mutex.actual_name == nume_client:
+			if address_client in mutecsii[name_mutex].process:
+				if my_mutex.actual_name == address_client:
 					send_message(socket, address_client, b'Ai dat Unlock')
-					next_guy = my_mutex.coada.pop(0)
-					my_mutex.actual_name = next_guy
-					send_message(socket, address_client, b'Ai dat Lock')
+
+					if my_mutex.coada:
+						next_guy = my_mutex.coada.pop(0)
+						my_mutex.actual_name = next_guy
+						send_message(socket, address_client, b'Ai dat Lock')
+					else:
+						my_mutex.actual_name = None
 				else:
 					send_message(socket, address_client, b'Nume client != nume actual')
 			else:
