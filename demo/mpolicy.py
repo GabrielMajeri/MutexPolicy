3f9f38b1915a0c3273a73faccaa6c51d63db8cf5
bogdangvr <45423416+bogdangvr@users.noreply.github.com>
2020-01-16 22:55:40 +0200

    Update mpolicy.py

--- demo/mpolicy.py
+++ demo/mpolicy.py
@@ -1,20 +1,22 @@
 import os
 import zmq
-
+import logging
 
 class MutexPolicy:
     def __init__(self, address="127.0.0.1"):
         "Connects to the mutex policy daemon running at the specified address"
+        logging.info("Connecting to mutex policy daemon...")
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REQ)
         self.socket.connect(f"tcp://{address}:5555")
+        logging.info("Connected to mutex policy daemon")
 
     def open(self, mutex_name):
         """Opens a mutex identified by an unique name.
-
         The caller must eventually call `close()` on the returned mutex to
         free up the resources used for it by the daemon.
         """
+        logging.info("Opening mutex...")
 
         msg = f"{os.getpid()} O {mutex_name}"
         self.socket.send_string(msg)
@@ -24,15 +26,16 @@ class MutexPolicy:
         if msg_rec != "Ok":
             raise Exception(f"Error: {msg_rec}")
 
+        logging.info("Mutex successfully opened")
         return Mutex(mutex_name, self.socket)
 
     def lst(self):
         """Returns a list representing all the mutexes
         currently open in the system."""
+        logging.info("Returning mutex list...")
         self.socket.send_string("list")
-
+        logging.info("Returned mutex list successfully")
         return self.socket.recv_string()
-
 
 class Mutex:
     def __init__(self, name, socket):
@@ -41,6 +44,7 @@ class Mutex:
 
     def close(self):
         "Close the given mutex "
+        logging.info("Closing mutex...")
 
         msg = f"{os.getpid()} C {self.name}"
         self.socket.send_string(msg)
@@ -49,9 +53,11 @@ class Mutex:
 
         if msg_rec != "Ok":
             raise Exception(f"Error: {msg_rec}")
+        logging.info("Mutex successfully closed")
 
     def lock(self):
         "Lock the mutex or blocks until we are able to lock it."
+        logging.info("Locking mutex...")
 
         msg = f"{os.getpid()} L {self.name}"
 
@@ -60,9 +66,11 @@ class Mutex:
 
         if msg_rec != "Ok":
             raise Exception(f"Error: {msg_rec}")
+        logging.info("Mutex successfully locked")
 
     def unlock(self):
         "Unlocks the mutex and allows the next process to take it."
+        logging.info("Unlocking mutex...")
 
         msg = f"{os.getpid()} U {self.name}"
 
@@ -71,3 +79,4 @@ class Mutex:
 
         if msg_rec != "Ok":
             raise Exception(f"Error: {msg_rec}")
+        logging.info("Mutex successfully unlocked")
