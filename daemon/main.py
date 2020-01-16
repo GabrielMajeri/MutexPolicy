c6a838bf1880915726303271e57533ef9c7a6fe4
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-01-16 18:44:20 +0200

    Rewrite messages in english

--- daemon/main.py
+++ daemon/main.py
@@ -22,11 +22,7 @@ def send_message(socket, address_client, message):
 
 def main():
 
-	logging.basicConfig(filename='../example.log',format='%(asctime)s %(message)s',level=logging.DEBUG)
-
-	#logging.debug('This message should go to the log file')
-	#logging.info('So should this')
-	#logging.warning('And this, too')
+	logging.basicConfig(format='%(asctime)s %(message)s',level=logging.DEBUG)
 
 	logging.info("Hello from daemon")
 
@@ -38,14 +34,14 @@ def main():
 	socket.bind("tcp://*:5555")
 
 	while True:
-		address_client, empty, msg = socket.recv_multipart() #nume client, empty, mesaj
+		address_client, empty, msg = socket.recv_multipart() # Client name, empty string, message
 
 		msg = msg.decode('UTF-8')
 
 		msg_split = msg.split()
 
 		if len(msg_split) == 1 and msg == "list":
-			logging.info("A fost afisata lista cu piduri!")
+			logging.info("Returning per-PID mutex list")
 
 			send_message(socket, address_client, str(pid_to_mutex).encode("UTF-8"))
 
@@ -53,17 +49,17 @@ def main():
 
 		pid, command, mutex_name = msg_split
 
-		logging.info("Pid-ul %s cu comanda %s si mutex nameul %s", pid, command, mutex_name)
+		logging.info("PID %s - Command %s - Mutex %s", pid, command, mutex_name)
 
 		my_mutex = mutexes[mutex_name]
 
 		if command == 'O': #Open
 			if mutex_name in pid_to_mutex[pid]:
-				logging.warning("Mutexul %s este deja in lista", mutex_name)
+				logging.warning("Mutex %s is already opened by process", mutex_name)
 
-				send_message(socket, address_client, b'Nu este Ok') #este in lista deja mutexul
+				send_message(socket, address_client, b'mutex is already opened') #este in lista deja mutexul
 			else:
-				logging.info("Pid-ul %s a deschis mutexul %s", pid, mutex_name)
+				logging.info("Process %s opened mutex %s", pid, mutex_name)
 
 				pid_to_mutex[pid].append(mutex_name)
 				my_mutex.process.add(pid)
@@ -73,72 +69,74 @@ def main():
 		elif command == 'C':	#Close
 			if mutex_name in pid_to_mutex[pid]:
 
-				logging.info("S-a sters mutexul %s", mutex_name)
+				logging.info("Closing mutex %s", mutex_name)
 
 				my_mutex.process.remove(pid)
-				del pid_to_mutex[pid]
+				pid_to_mutex[pid].remove(mutex_name)
+				if not pid_to_mutex[pid]:
+					del pid_to_mutex[pid]
 
 				send_message(socket, address_client, b'Ok') #s-a sters
 
 				if not my_mutex.process:
+					logging.info("Releasing memory for mutex %s", mutex_name)
 					del mutexes[mutex_name]
 
 			else:
-				logging.error("Mutexul %s nu exista!", mutex_name)
+				logging.error("Mutex %s does not exist", mutex_name)
 
-				send_message(socket, address_client, b'Nu exista adresa specificata')
+				send_message(socket, address_client, b'Mutex does not exist')
 
 		elif command == 'L':	#Lock
 			if mutex_name in pid_to_mutex[pid]:
 				if my_mutex.owner_pid == None:
 					my_mutex.owner_pid = pid
 
-					logging.info("Pid-ul %s a dat lock la mutexul %s", pid, mutex_name)
+					logging.info("Process %s locked mutex %s", pid, mutex_name)
 
 					send_message(socket, address_client, b'Ok') # Ai dat lock
 				elif my_mutex.owner_pid == pid:
-					logging.warning("Pid-ul %s deja a dat lock!", pid)
+					logging.warning("Process %s already locked mutex %s!", pid, mutex_name)
 
-					send_message(socket, address_client, b'Ai dat lock de doua ori!')
+					send_message(socket, address_client, b'Cannot lock mutex twice')
 				else:
-					logging.info("Pid-ul %s a fost adaugat in coada", pid)
+					logging.info("Process %s added to waiting queue", pid)
 
 					my_mutex.queue.append((pid, address_client))
 			else:
-				logging.error("Mutexul %s nu exista!", mutex_name)
+				logging.error("Mutex %s does not exist", mutex_name)
 
-				send_message(socket, address_client, b'Nu exista adresa specificata')
+				send_message(socket, address_client, b'Mutex does not exist')
 
 		elif command == 'U':	#Unlock
 			if mutex_name in pid_to_mutex[pid]:
 				if my_mutex.owner_pid == pid:
-					logging.info("Pid-ul %s a dat unlock", pid)
+					logging.info("Process %s unlocked mutex %s", pid, mutex_name)
 
-					send_message(socket, address_client, b'Ok') # Ai dat lock
+					send_message(socket, address_client, b'Ok') # A dat unlock
 
 					if my_mutex.queue:
 						next_guy, address_client = my_mutex.queue.pop(0)
 						my_mutex.owner_pid = next_guy
 
-						logging.info("Pid-ul %s a luat lock", my_mutex.owner_pid)
+						logging.info("Process %s locked mutex %s", my_mutex.owner_pid, mutex_name)
 
-						send_message(socket, address_client, b'Ok') # Ai dat lock
+						send_message(socket, address_client, b'Ok') # A dat lock
 					else:
 						my_mutex.owner_pid = None
 				else:
-					logging.warning("Owner_pid %s != actual pid %s", my_mutex.owner_pid, pid)
+					logging.warning("Process %s tried to unlock mutex, already locked by process %s", pid, my_mutex.owner_pid)
 
-					send_message(socket, address_client, b'Nume client != nume actual')
+					send_message(socket, address_client, b'Cannot unlock mutex if not locked')
 			else:
-				logging.warning("Mutexul %s nu exista!", mutex_name)
+				logging.warning("Mutex %s does not exist!", mutex_name)
 
-				send_message(socket, address_client, b'Nu exista adresa specificata')
+				send_message(socket, address_client, b'Inexistent mutex')
 
 		else:
-			print("Comanda necunoscuta:", msg)
-			logging.warning("Nu ai ales nici o optiune buna. Incearca O/C/L/U")
+			logging.warning("Unknown command: %s", msg)
 
-			send_message(socket, address_client, b'Nu ai ales bine. Mai incearca')
+			send_message(socket, address_client, b'Unknown command')
 
 
 if __name__ == '__main__':
